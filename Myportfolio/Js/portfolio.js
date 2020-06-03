fa5c15a4d532ba953fd26e5aeea6f67aefb5f124
Wilson M. Cabanlit <cabanlit7@gmail.com>
2020-06-03 10:25:46 +0800

    Add files via upload

---
+++ Myportfolio/Js/portfolio.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('load', function () {
+    //your script
+    new Vue({
+        el: '#app',
+        data:{
+            message:'Hello Word!',
+            vissible: false
+        }
+        
+    })
+})
